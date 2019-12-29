dd12936143fa22769b05a6704e5db339a1ebefc6
Tomislav Gudelj <tomislav.gudelj@gmail.com>
2019-12-29 17:37:44 +0100

    Work in progress

---
+++ OSB.Core/UpdateChecker.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OSB.Core {
+    public class UpdateChecker {
+        public async void Check() {
+            WebClient client = new WebClient();
+            try
+            {
+                string updateInfoString = await client.DownloadStringTaskAsync("https://matricapp.com/update.json");
+                UpdateInfo info = JsonConvert.DeserializeObject<UpdateInfo>(updateInfoString);
+                Version version = Assembly.GetExecutingAssembly().GetName().Version;
+                //Compare update info and current version
+                if (version.Major < info.update.majorVersion)
+                {
+                    info.updateAvailable = true;
+                }
+                else
+                {
+                    if (version.Major <= info.update.majorVersion && version.Minor < info.update.minorVersion)
+                    {
+                        info.updateAvailable = true;
+                    }
+                }
+                if (callback != null)
+                {
+                    await callback.ExecuteAsync(new object[] { info });
+                }
+            }
+            catch
+            {
+                if (callback != null)
+                {
+                    await callback.ExecuteAsync(new object[] { null });
+                }
+            }
+        }
+
+    }
+}
---
+++ OSB.Core/UpdateInfo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OSB.Core {
+    public class UpdateInfo {
+        public int betaCodeVersion;
+        public int productionCodeVersion;
+        public string betaDownloadUrl;
+        public string productionDownloadUrl;
+
+    }
+}
